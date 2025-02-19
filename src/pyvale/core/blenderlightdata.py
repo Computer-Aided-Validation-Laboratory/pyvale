dd285e9db79e77f07d1fe73909dffe72f458e793
lornasibson <lorna.sibson@ukaea.uk>
2025-02-19 14:05:15 +0000

    BlenderLightData class moved into core pyvale

---
+++ src/pyvale/core/blenderlightdata.py
@@ -0,0 +1,28 @@
+"""
+================================================================================
+pyvale: the python validation engine
+License: MIT
+Copyright (C) 2024 The Computer Aided Validation Team
+================================================================================
+"""
+from dataclasses import dataclass
+from enum import Enum
+import numpy as np
+from scipy.spatial.transform import Rotation
+
+class BlenderLightType(Enum):
+    POINT = 'POINT'
+    SUN = 'SUN'
+    SPOT = 'SPOT'
+    AREA = 'AREA'
+
+@dataclass
+class BlenderLightData():
+    type: BlenderLightType | None = None
+    pos_world: np.ndarray
+    rot_world: Rotation
+    energy: int
+
+    def __post_init__(self) -> None:
+        self.type = BlenderLightType.POINT
+
