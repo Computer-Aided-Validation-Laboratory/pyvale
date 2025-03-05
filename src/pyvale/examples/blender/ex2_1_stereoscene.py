6026391b64b8a2951856b4fc14d5c1b6760b8baf
lornasibson <lorna.sibson@ukaea.uk>
2025-03-05 15:04:40 +0000

    Stereo Blender examples added

--- src/pyvale/examples/blender/ex2_1_stereoscene.py
+++ src/pyvale/examples/blender/ex2_1_stereoscene.py
@@ -29,14 +29,29 @@ def main() -> None:
     part_rotation = Rotation.from_euler("xyz", [0, 0, 0])
     pyvale.BlenderTools.rotate_blender_part(part=part, rot_world=part_rotation)
 
-    # Add the camera
-    cam_data = pyvale.CameraData(pixels_num=np.array([2464, 2056]),
+    # Add the stereo camera system
+    cam_data_0 = pyvale.CameraData(pixels_num=np.array([2464, 2056]),
                                  pixels_size=np.array([3.45, 3.45]),
-                                 pos_world=(0, 0, 250),
+                                 pos_world=np.array([0, 0, 250]),
                                  rot_world=Rotation.from_euler("xyz", [0, 0, 0]),
                                  roi_cent_world=(0, 0, 0),
                                  focal_length=15.0)
-    camera = pyvale.BlenderScene.add_camera(cam_data)
+    # Set this to "symmetric" to get a symmetric stereo system or set this to
+    # "faceon" to get a face-on stereo system
+    stereo_system = "symmetric"
+    if stereo_system == "symmetric":
+        cam_data_1 = pyvale.blender_symmetric_stereo(cam_data_0=cam_data_0,
+                                                 base=35.0,
+                                                 stereo_angle=7.0)
+    if stereo_system == "faceon":
+        cam_data_1 = pyvale.blender_faceon_stereo(cam_data_0=cam_data_0,
+                                                 base=35.0,
+                                                 stereo_angle=7.0)
+    # Generate calibration file
+    stereo_data = pyvale.CameraStereoData(cam_data_0, cam_data_1)
+    calib_filepath = Path.cwd() / 'src/pyvale/data/'
+    pyvale.BlenderTools.generate_calib_file(stereo_data, calib_filepath)
+
 
     # Add the light
     light_data = pyvale.BlenderLightData(type=pyvale.BlenderLightType.POINT,
@@ -52,30 +67,24 @@ def main() -> None:
     pyvale.BlenderScene.add_speckle(part=part,
                                     speckle_path=speckle_path,
                                     mat_data=material_data,
-                                    cam_data=cam_data)
+                                    cam_data=cam_data_0)
 
-    # Deform and render images
+    # Rendering image
     # --------------------------------------------------------------------------
-    # Set this to True to render image of the deforming part
-    render_images = True
-    if render_images is True:
-        save_dir = Path('/home/lorna/pyvale/dev/lsdev/rendered_images/deform')
+    # Set this to True to render image of the current scene
+    render_image = True
+    if render_image is True:
+        save_dir = Path('/home/lorna/pyvale/dev/lsdev/rendered_images/stereo')
         save_name = 'test'
-        render_data = pyvale.RenderData(cam_data=cam_data,
+        render_data = pyvale.RenderData(cam_data=(cam_data_0, cam_data_1),
                                         save_dir=save_dir,
                                         save_name=save_name)
 
-        pyvale.BlenderScene.render_deformed_images(sim_data=sim_data,
-                                                   render_data=render_data,
-                                                   part=part,
-                                                   save=True)
-    elif render_images is False:
-        pyvale.BlenderScene.deform_all_timesteps(sim_data, part)
+        pyvale.BlenderScene.render_single_image(save=True, render_data=render_data)
 
     # Save Blender file
     # --------------------------------------------------------------------------
-    # TODO: Work out where to automatically save things
-    blender_path = Path('/home/lorna/pyvale/dev/lsdev/blender_files/test_deform.blend')
+    blender_path = Path('/home/lorna/pyvale/dev/lsdev/blender_files/test_core.blend')
     pyvale.BlenderTools.save_blender_file(blender_path, override=True)
 
 if __name__ == "__main__":
