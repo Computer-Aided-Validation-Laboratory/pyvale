1cde046030249feeecd11262739a549a335cee30
Lorna Sibson <lorna.sibson@ukaea.uk>
2025-03-26 11:01:58 +0000

    Blender examples downsized

--- src/pyvale/examples/blender/ex1_1_blenderscene.py
+++ src/pyvale/examples/blender/ex1_1_blenderscene.py
@@ -30,26 +30,27 @@ def main() -> None:
     pyvale.BlenderTools.rotate_blender_part(part=part, rot_world=part_rotation)
 
     # Add the camera
-    cam_data = pyvale.CameraData(pixels_num=np.array([2464, 2056]),
+    cam_data = pyvale.CameraData(pixels_num=np.array([1540, 1040]),
                                  pixels_size=np.array([0.00345, 0.00345]),
                                  pos_world=(0, 0, 400),
                                  rot_world=Rotation.from_euler("xyz", [0, 0, 0]),
                                  roi_cent_world=(0, 0, 0),
-                                 focal_length=20)
+                                 focal_length=15)
     camera = pyvale.BlenderScene.add_camera(cam_data)
 
     # Add the light
     light_data = pyvale.BlenderLightData(type=pyvale.BlenderLightType.POINT,
-                                         pos_world=(0, 0, 200),
+                                         pos_world=(0, 0, 400),
                                          rot_world=Rotation.from_euler("xyz",
                                                                        [0, 0, 0]),
-                                         energy=400 * 10**3)
+                                         energy=1 * 10**6)
     light = pyvale.BlenderScene.add_light(light_data)
 
     # Apply the speckle pattern
     material_data = pyvale.BlenderMaterialData()
-    # speckle_path = pyvale.DataSet.dic_pattern_5mpx_path()
-    speckle_path = Path.cwd() / 'dev/lsdev/speckle_3000.bmp'
+    speckle_path = pyvale.DataSet.dic_pattern_5mpx_path()
+    # NOTE: If you wish to use a bigger camera, you will need to generate a
+    # bigger speckle pattern generator
     pyvale.BlenderScene.add_speckle(part=part,
                                     speckle_path=speckle_path,
                                     mat_data=material_data,
@@ -60,11 +61,12 @@ def main() -> None:
     # Set this to True to render image of the current scene
     render_image = True
     if render_image is True:
-        save_dir = Path.cwd() / 'src/pyvale/data/blender_images'
+        save_dir = Path.cwd() / 'src/pyvale/data/blender/blender_images'
         save_name = 'ex_1_1'
         render_data = pyvale.RenderData(cam_data=cam_data,
                                         save_dir=save_dir,
-                                        save_name=save_name)
+                                        save_name=save_name,
+                                        samples=1)
 
         pyvale.BlenderScene.render_single_image(save=True, render_data=render_data)
 
