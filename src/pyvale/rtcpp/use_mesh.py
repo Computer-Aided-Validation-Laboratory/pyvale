9717b87c591050c283a88696168f23423bc07322
John <coolmule0@hotmail.co.uk>
2025-04-16 13:03:48 +0100

    a mesh import

--- src/pyvale/rtcpp/use_mesh.py
+++ src/pyvale/rtcpp/use_mesh.py
@@ -13,6 +13,7 @@ def IsRectangle(a, b, c, d) -> bool:
     return IsOrthogonal(a, b, c) and IsOrthogonal(b, c, d) and IsOrthogonal(c, d, a)
 
 def IsRectangleAnyOrder(a, b, c, d):
+    # returns the ordering which makes a rectangle if there is one
     if IsRectangle(a, b, c, d):
         return (a, b, c, d)
     if IsRectangle(b, c, a, d):
@@ -23,10 +24,21 @@ def IsRectangleAnyOrder(a, b, c, d):
         return None
     # return IsRectangle(a, b, c, d) or IsRectangle(b, c, a, d) or IsRectangle(c, a, b, d)
 
+def get_mesh_spat_dim(sim_data: mh.SimData) -> int:
+    spat_dim = 2 # all values in z are 0
+    nodes = sim_data.coords
+    check_if_2d = np.count_nonzero(nodes, axis=0)
+    if check_if_2d[2] == 0:
+        spat_dim = 2
+    else:
+        spat_dim = 3
+    return spat_dim
 
-data_path = pyvale.DataSet.thermal_2d_output_path()
+
+data_path = pyvale.DataSet.thermal_2d_path()
 sim_data = mh.ExodusReader(data_path).read_all_sim_data()
 field_key = "temperature"
+spat_dim = get_mesh_spat_dim(sim_data)
 # Scale and move to make 3D visualisation scaling easier
 sim_data.coords = sim_data.coords*6000.0 - np.array([-300, -200, -200])
 
@@ -37,28 +49,37 @@ z_lims = (0.0,0.0)
 sens_pos = pyvale.create_sensor_pos_array(n_sens,x_lims,y_lims,z_lims)
 sens_data = pyvale.SensorData(positions=sens_pos)
 
-conne = sim_data.connect["connect1"]
-quad_dicts = []
+(pv_grid, _) = pyvale.simdata_to_pyvista(sim_data,
+                                                ["temperature"],
+                                                spat_dim)
+pv_surf =  pv_grid.extract_surface()
+
+
+vertices = pv_surf.points
+
+def get_faces(pv_surf):
+    # read in first number of faces
+    faces = []
+    index = 0
+    while index < len(pv_surf.faces):
+        num_faces = pv_surf.faces[index]
+        faces.append(pv_surf.faces[index+1:index+num_faces+1])
+        index += num_faces+1
+    return faces
+
+faces = get_faces(pv_surf)
+
 missed = 0
 count = 0
-# loop through 
-for i, _ in enumerate(conne[0]):
-    # get the ids
-    i0 = conne[0][i] - 1
-    i1 = conne[1][i] - 1
-    i2 = conne[2][i] - 1
-    i3 = conne[3][i] - 1
-
-    # get the locs
-    p0 = sim_data.coords[i0]
-    p1 = sim_data.coords[i1]
-    p2 = sim_data.coords[i2]
-    p3 = sim_data.coords[i3]
+quad_dicts = []
+for f in faces:
+    indices = f
+    locs = vertices[f]
 
     # Are they rectangles?
-    if IsRectangleAnyOrder(p0, p1, p2, p3) is not None:
+    if IsRectangleAnyOrder(locs[0], locs[1], locs[2], locs[3]) is not None:
         count += 1
-        a, b, c, d = IsRectangleAnyOrder(p0, p1, p2, p3)
+        a, b, c, d = IsRectangleAnyOrder(locs[0], locs[1], locs[2], locs[3])
     else:
         missed += 1
         continue
@@ -66,9 +87,10 @@ for i, _ in enumerate(conne[0]):
     quad_dict = {
         "Q": point3(a[0], a[1], a[2]),
         "u": vec3(b[0]-a[0], b[1]-a[1], b[2]-a[2]),
-        "v": vec3(c[0]-a[0], c[1]-a[1], c[2]-a[2]),
+        "v": vec3(d[0]-a[0], d[1]-a[1], d[2]-a[2]),
     }
     quad_dicts.append(quad_dict)
+
 print("missed: ", missed)
 print("count: ", count)
 
