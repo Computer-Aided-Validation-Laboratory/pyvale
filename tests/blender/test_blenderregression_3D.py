4660515a354d397951c2f559c01c8331fde0a14b
Joel Hirst <jrhirst100@aol.com>
2025-08-01 12:02:05 +0100

    added small tolerance for output arrays in blender tests.

--- tests/blender/test_blenderregression_3D.py
+++ tests/blender/test_blenderregression_3D.py
@@ -90,7 +90,7 @@ def test_stereo_convenience_cameras(placement, output, request, sample_scene_no_
                                                           render_data=render_data)
     output = request.getfixturevalue(output)
 
-    npt.assert_allclose(image_array, output, atol=1)
+    npt.assert_allclose(image_array, output, atol=2, rtol=0)
 
 def test_stereo_deformation(sample_stereo_scene, deformed_images, tmp_path):
     (stereo_system, part, render_mesh, scene) = sample_stereo_scene
@@ -103,7 +103,7 @@ def test_stereo_deformation(sample_stereo_scene, deformed_images, tmp_path):
                                                               part=part,
                                                               stage_image=True)
     image_array = image_arrays[:, :, 120:]
-    npt.assert_allclose(image_array, deformed_images, atol=1)
+    npt.assert_allclose(image_array, deformed_images, atol=2, rtol=0)
 
 def test_cal_images():
     calibration_data = pyvale.CalibrationData(angle_lims=(-10, 10),
