88130ba0d875968d8f0097bebeb11c1eae16431a
ScepticalRabbit <thescepticalrabbit@gmail.com>
2025-08-02 15:22:19 +0100

    Fixed example 8 of mooseherder

--- src/pyvale/examples/mooseherder/ex8_read_existing_sweep_output.py
+++ src/pyvale/examples/mooseherder/ex8_read_existing_sweep_output.py
@@ -9,7 +9,8 @@ Reading results from a pre-run parameter sweep
 ================================================================================
 
 In this example we will read in the output of a parameter sweep performed by the
-'herd' workflow manager.
+'herd' workflow manager specifically showing how we can read a sweep if we don't
+have the original directory manager.
 
 **NOTE**: this example assumes you have run one of the previous examples using
 the herd workflow manager and that there is a series of output simulation
@@ -40,7 +41,8 @@ dir_manager.set_base_dir(output_base_path)
 # the output in parallel will probably be slower than reading sequentially so
 # make sure you test the reader for your particular case.
 #
-# We then use the reader check for
+# We then use the reader extract the list of list of output file paths and the
+# combinations of variables that were used for this parameter sweep.
 sweep_reader = SweepReader(dir_manager,num_para_read=4)
 
 output_files = sweep_reader.read_all_output_file_keys()
@@ -55,18 +57,16 @@ pprint(sweep_reader.read_all_sweep_var_files())
 print()
 
 #%%
-#
-
-print("-"*80)
-print('Reading all output files in parallel as list(SimData).')
+# Now we use our sweep reader to read in the existing sweep results. We can also
+# use a read configuration here if we want to filter out specific parts of the
+# simulation output but for now we will read all of the data.
 
 if __name__ == '__main__':
     start_time = time.perf_counter()
     sweep_data = sweep_reader.read_results_para()
     read_time_para = time.perf_counter() - start_time
 
+print("-"*80)
 print(f'Number of simulations outputs read: {len(sweep_data):d}')
-print()
-print("="*80)
 print(f'Read time parallel   = {read_time_para:.6f} seconds')
-print("="*80)
+print("-"*80)
