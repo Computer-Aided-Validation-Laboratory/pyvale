1ed966472bd365823fc94de9296424b6f01b9eab
meganasampson <megan.anne.sampson@gmail.com>
2025-10-03 15:55:09 +0100

    added draft for tests

--- tests/sensorsim/test_traceplotter.py
+++ tests/sensorsim/test_traceplotter.py
@@ -1,4 +1,10 @@
 import pytest
 import pytest_mock
+import pyvale.sensorsim as sens
+import matplotlib.pyplot as plt
 
-# later ...
+def drafttest_set_subplotsize(mocker):
+    field_key: str = "temperature"
+    mock_sensor_data = mocker.Mock()
+    mock_sensor_data.tc_array = "whatever this is"
+    sens.plot_time_traces(mock_sensor_data.tc_array, field_key)
