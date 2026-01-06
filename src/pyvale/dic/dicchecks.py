6736426a7fd46c911daac0aa2edfbec5bb7ee411
JoelPhys <jrhirst100@aol.com>
2026-01-06 14:58:41 +0000

    fix for dimension check of reference image against roi mask.

--- src/pyvale/dic/dicchecks.py
+++ src/pyvale/dic/dicchecks.py
@@ -402,10 +402,14 @@ def check_and_get_images(reference: np.ndarray | str | Path,
             def_arr = def_arr.reshape((1,def_arr.shape[0],def_arr.shape[1]))
 
         elif (reference.shape != deformed[0].shape or reference.shape != roi.shape):
-            raise ValueError(f"Shape mismatch: reference {reference.shape}, "
-                             f"deformed[0] {deformed[0].shape}, roi {roi.shape}")
-        
+            raise ValueError(f"Shape mismatch: reference={reference.shape}, "
+                             f"deformed[0]={deformed[0].shape}, roi={roi.shape}")
 
+        # check ROI dimensions agrees with reference image
+        if (reference.shape != roi.shape):
+            raise ValueError(f"Shape mismatch: reference={reference.shape}, "
+                             f"roi={roi.shape}")
+ 
         # need to set some dummy filenames in the case that the user passes numpy arrays
         filenames = [f"deformed image {i}" for i in range(def_arr.shape[0])]
     
