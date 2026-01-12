5908b944885377651727833f5cda4117900dcc10
Chris Dawson <chrisdawson1996@gmail.com>
2026-01-12 15:13:27 +0000

    pull over more of the main loop, eg equivalent stress/plastic multiplier/plastic criterion

--- src/pyvale/vfm/main.py
+++ src/pyvale/vfm/main.py
@@ -16,9 +16,14 @@ import numpy as np
 # possible array axes: c11, c12, c22, time
 # or possibly: x, y, (c11, c12, c22), timestep <- but do we need x and y? Would be nice to have for
 # intuition but would this reduce performance?
+#
+# Data should be 23 slices x 35708 rows x 3 cols
+# Col index is last index, and row index is second to last
 
 strain_data = loadmat("/Users/chris/work/vfmap-numerical-paper/scripts/strain.mat")
-spatial_param_data = loadmat("/Users/chris/work/vfmap-numerical-paper/scripts/spatialParamData.mat")
+spatial_param_data = loadmat(
+    "/Users/chris/work/vfmap-numerical-paper/scripts/spatialParamData.mat"
+)
 
 # Contains componenets c11, c12, c22 arrays with 35708 values per column and 23 columns (timesteps)
 strain = strain_data["strain"]
@@ -27,8 +32,14 @@ c11 = strain["c11"][0][0]
 c12 = strain["c12"][0][0]
 c22 = strain["c22"][0][0]
 
-# 23 timesteps x 3 components x 35708 values
-strain = np.stack((c11, c22, c12), axis=2).transpose((1, 2, 0))
+# 23 timesteps x 35708 values x 3 components
+strain = np.stack((c11, c22, c12), axis=2).transpose((1, 0, 2))
+
+# Output stresses
+sigma_xx = np.zeros((35708, 23))
+sigma_xy = np.zeros((35708, 23))
+sigma_yy = np.zeros((35708, 23))
+von_mises_stress = np.zeros((35708, 23))
 
 young_modulus = 190000.0
 # Poisson's ratio
@@ -46,22 +57,62 @@ incremental_strain[1:, :, :] = np.diff(strain, axis=0)
 stress = np.empty_like(strain)
 # PEEQ
 equivalent_plastic_strain = np.empty((35708, 23))
-yield_strength = spatial_param_data["spatialParamData"]["param3"][0][0]["parameterMap"][0][0]
-hardening_modulus = spatial_param_data["spatialParamData"]["param4"][0][0]["parameterMap"][0][0]
+yield_strength = spatial_param_data["spatialParamData"]["param3"][0][0]["parameterMap"][
+    0
+][0]
+hardening_modulus = spatial_param_data["spatialParamData"]["param4"][0][0][
+    "parameterMap"
+][0][0]
 
 num_timesteps = 23
-for i in range(num_timesteps):
+for t in range(num_timesteps):
     # Convert shear strain component from tensorial shear strain to engineering shear strain
     # Since by convention tensorial shear strain is half of engineering shear strain
-    incremental_strain[i, 2, :] *= 2
+    incremental_strain[t, :, 2] *= 2
 
-    if i == 0:
-        stress[0, :, :] = incremental_strain[0, :, :].T.dot(elasticity_matrix).T
+    if t == 0:
+        stress[0, :, :] = incremental_strain[0, :, :].dot(elasticity_matrix)
         # Flatten with order F to use column major ordering same as matlab
-        yield_stress_vec = yield_strength.flatten(order="F") + (hardening_modulus.flatten(order="F") * equivalent_plastic_strain[:, 0])
+        yield_stress = yield_strength.flatten(order="F") + (
+            hardening_modulus.flatten(order="F") * equivalent_plastic_strain[:, 0]
+        )
     else:
-        prev_stress = stress[i-1, :, :]
-        stress[i, :, :] = prev_stress + (incremental_strain[i, :, :].T.dot(elasticity_matrix).T)
-        yield_stress_vec = yield_strength.flatten(order="F") + (hardening_modulus.flatten(order="F") * equivalent_plastic_strain[:, i-1])
-        equivalent_plastic_strain[:, i] = equivalent_plastic_strain[:, i-1]
+        prev_stress = stress[t - 1, :, :]
+        stress[t, :, :] = prev_stress + (
+            incremental_strain[t, :, :].dot(elasticity_matrix)
+        )
+        yield_stress = yield_strength.flatten(order="F") + (
+            hardening_modulus.flatten(order="F") * equivalent_plastic_strain[:, t - 1]
+        )
+        equivalent_plastic_strain[:, t] = equivalent_plastic_strain[:, t - 1]
 
+    # Check yield criterion
+    equivalent_stress = np.zeros(35708)
+    equivalent_stress[:] = (1 / 3) * (
+        stress[t, :, 0] ** 2
+        + stress[t, :, 1] ** 2
+        - stress[t, :, 0] * stress[t, :, 1]
+        + (3 * stress[t, :, 2] ** 2)
+    )
+
+    yield_criterion_check = equivalent_stress > (1 / 3 * (yield_stress**2))
+
+    plasticity_mask = yield_criterion_check
+    elasticity_mask = np.logical_not(yield_criterion_check)
+
+    # Update elastic stresses
+    sigma_xx[plasticity_mask, t] = stress[t, plasticity_mask, 0]
+    sigma_yy[plasticity_mask, t] = stress[t, plasticity_mask, 1]
+    sigma_xy[plasticity_mask, t] = stress[t, plasticity_mask, 2]
+
+    plastic_multiplier = np.zeros(35708)
+    plastic_multiplier[plasticity_mask] = (
+        (1 / 6) * np.sum(stress[t, plasticity_mask, 0:2], axis=1) ** 2
+        + 0.5 * (stress[t, plasticity_mask, 1] - stress[t, plasticity_mask, 0]) ** 2
+        + 2 * (stress[t, plasticity_mask, 2]) ** 2
+    )
+
+    plastic_criterion = np.zeros(35708)
+    plastic_criterion[plasticity_mask] = (1 / 2) * plastic_multiplier[
+        plasticity_mask
+    ] - (1 / 3) * (yield_stress[plasticity_mask] ** 2)
