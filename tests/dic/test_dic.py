7c027562fa821bb32f37cd1efaa1d50ec0f3408e
JoelPhys <jrhirst100@aol.com>
2026-01-13 11:51:43 +0000

    Updated DIC tests to check scaling and scaling+offset for NSSD and ZNSSD respectively.

--- tests/dic/test_dic.py
+++ tests/dic/test_dic.py
@@ -10,10 +10,11 @@ copyright (c) 2024 the computer aided validation team
 import os
 os.environ['OMP_NUM_THREADS'] = '1'
 
+from PIL import Image
 import numpy as np
 import pyvale.dic as dic
 import pyvale.dataset as pyv_data
-
+from pathlib import Path
 
 test_dir = os.path.dirname(__file__)
 
@@ -37,6 +38,88 @@ true_00_5 = np.loadtxt(true_file_00_5, skiprows=1, delimiter=',')
 true_01_0 = np.loadtxt(true_file_01_0, skiprows=1, delimiter=',')
 true_25_0 = np.loadtxt(true_file_25_0, skiprows=1, delimiter=',')
 true_50_0 = np.loadtxt(true_file_50_0, skiprows=1, delimiter=',')
+
+
+
+
+# Create a deformed image where intensities are scaled/offset
+ref_image = Image.open(ref_pattern)
+files = list(def_pattern.parent.glob(def_pattern.name))
+def_image = Image.open(files[0])
+ref_arr = np.array(ref_image)
+def_arr = np.array(def_image)
+original_dtype = def_arr.dtype
+scale = 0.5
+offset = 50
+def_arr = def_arr.astype(np.float32)
+def_arr_scaled = def_arr * scale
+def_arr_scaled_offset = def_arr * scale + offset
+def_arr = def_arr.astype(original_dtype)
+def_arr_scaled = def_arr_scaled.astype(original_dtype)
+def_arr_scaled_offset = def_arr_scaled_offset.astype(original_dtype)
+
+
+def test_ssd_rigid():
+    dic.calculate_2d(reference=ref_arr,
+                     deformed=def_arr,
+                     roi_mask=roi.mask,
+                     seed=[250,250],
+                     subset_size=31,
+                     subset_step=15,
+                     max_displacement=2,
+                     opt_precision=1e-10,
+                     correlation_criteria="SSD",
+                     shape_function="AFFINE",
+                     method="MULTIWINDOW_RG",
+                     output_basepath=test_dir,
+                     output_prefix="test_ssd_rigid_")
+
+    output_file_00_5 = os.path.abspath(os.path.join(test_dir, "./test_ssd_rigid_def_img_0.csv"))
+    output_data_00_5 = np.loadtxt(output_file_00_5, skiprows=1, delimiter=',')
+    np.testing.assert_allclose(true_00_5[:, :6], output_data_00_5[:, :6], rtol=0.005, atol=1e-6,
+                               err_msg="Results from test Do not match ground truth displacement of 0.5px")
+
+    os.remove(output_file_00_5)
+
+def test_nssd_scaled_image_rigid():
+    dic.calculate_2d(reference=ref_arr,
+                     deformed=def_arr_scaled,
+                     roi_mask=roi.mask,
+                     seed=[250,250],
+                     subset_size=31,
+                     subset_step=15,
+                     max_displacement=2,
+                     correlation_criteria="NSSD",
+                     shape_function="AFFINE",
+                     method="MULTIWINDOW_RG",
+                     output_basepath=test_dir,
+                     output_prefix="test_nssd_scaled_image_rigid_")
+
+    output_file_00_5 = os.path.abspath(os.path.join(test_dir, "./test_nssd_scaled_image_rigid_def_img_0.csv"))
+    output_data_00_5 = np.loadtxt(output_file_00_5, skiprows=1, delimiter=',')
+    np.testing.assert_allclose(true_00_5[:, :7], output_data_00_5[:, :7], rtol=0.005, atol=1e-6,
+                               err_msg="Results from test Do not match ground truth displacement of 0.5px")
+    os.remove(output_file_00_5)
+
+def test_znssd_scaled_offset_image_rigid():
+    dic.calculate_2d(reference=ref_arr,
+                     deformed=def_arr_scaled_offset,
+                     roi_mask=roi.mask,
+                     seed=[250,250],
+                     subset_size=31,
+                     subset_step=15,
+                     max_displacement=2,
+                     correlation_criteria="ZNSSD",
+                     shape_function="AFFINE",
+                     method="MULTIWINDOW_RG",
+                     output_basepath=test_dir,
+                     output_prefix="test_znssd_scaled_offset_image_rigid_")
+
+    output_file_00_5 = os.path.abspath(os.path.join(test_dir, "./test_znssd_scaled_offset_image_rigid_def_img_0.csv"))
+    output_data_00_5 = np.loadtxt(output_file_00_5, skiprows=1, delimiter=',')
+    np.testing.assert_allclose(true_00_5[:, :7], output_data_00_5[:, :7], rtol=0.005, atol=1e-6,
+                               err_msg="Results from test Do not match ground truth displacement of 0.5px")
+    os.remove(output_file_00_5)
 
 def test_image_scan_znssd_affine():
     dic.calculate_2d(reference=ref_pattern,
