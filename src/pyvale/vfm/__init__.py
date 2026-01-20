9eda1dfa40b6dc2b0ed5bc2850848e6b2c3a86ce
Chris Dawson <chrisdawson1996@gmail.com>
2026-01-20 13:45:17 +0000

    add initial radial return test

---
+++ src/pyvale/vfm/__init__.py
@@ -0,0 +1,10 @@
+#===============================================================================
+# pyvale: the python validation engine
+# License: MIT
+# Copyright (C) 2025 The Computer Aided Validation Team
+#===============================================================================
+
+from .radial_return import radial_return, MaterialProperties
+
+__all__ = ["radial_return",
+           "MaterialProperties"]
