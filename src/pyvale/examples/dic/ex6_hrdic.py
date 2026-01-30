ff4e9a5cbf64c9daff7f01f023d9129c7f8c05a5
JoelPhys <jrhirst100@aol.com>
2026-01-30 16:00:28 +0000

    finished HRDIC example.

--- src/pyvale/examples/dic/ex6_hrdic.py
+++ src/pyvale/examples/dic/ex6_hrdic.py
@@ -26,6 +26,7 @@ HRDIC
 # pyvale modules
 import pyvale.dic as dic
 import matplotlib.pyplot as plt
+import numpy as np
 
 # %%
 # Because of the size of the images, we'll avoid using the interactive GUI for this example.
@@ -44,7 +45,8 @@ roi.rect_boundary(left=0,right=0,top=0,bottom=0)
 roi.seed = [5000,5000]
 
 # %%
-# We can then run the DIC engine using our ROI mask and seed location
+# We can then run the DIC engine using our ROI mask and seed location.
+#
 
 dic.calculate_2d(reference="ref.tiff",
                  deformed="def.tiff",
@@ -57,27 +59,37 @@ dic.calculate_2d(reference="ref.tiff",
                  fft_mad_scale=3.0,
                  correlation_criteria="ZNSSD",
                  shape_function="AFFINE",
-                 precision=0.001,
                  threshold=0.8,
                  output_basepath="./")
 
+# %% 
+#  There's a couple of things to pay attention to here:
+#
+#  * :code:`max_displacement`: Sets the assumed maximum displacment. It is better
+#    to overestimate rather than underestimate this value
+#  * :code:`fft_mad` and :code:`fft_mad_scale`: These remove outliers by
+#    calculating a Mean Absolute Deviation (MAD) at each stage of the FFT windowing
+#    and prevent the propagation of incorrect rigid body estimates down through the window sizes.
+#    The mad scale value determines how strict the outlier removal is - larger values of 
+#    make the criterion more permissive, whereas smaller values more aggressively suppress deviations.
+#  * :code:`threshold`: Sets the minimum correlation coefficient value for a
+#    subset to be considered a good match. Any correlation coefficients less than
+#    this value will not be present in the results. If you want to have all
+#    values present in the output, you can set
+#    :code:`output_below_threshold=True`.
+#
+#  **This might take some time to run depending on the amount of available
+#  threads! You can manually set this with the** :code:`num_threads` **argument.**
+
 # %%
-# If you saved the results in a human-readable format, you can use any tool
-# (e.g., Excel, Python, MATLAB) for post-processing.
-#
-# For convenience, we provide a utility function to import results back into Python
-# for analysis and visualization: :func:`pyvale.dic.import_2d`.
-#
-# The returned object is an instance of :class:`pyvale.DICResults`. If the results
-# were saved in binary format or with a custom delimiter, be sure to specify those parameters.
+# The results can be imported in a standard way:
 dic_files = "dic_results_*.csv"
 dicdata = dic.import_2d(data=dic_files)
 
 # %%
-# As an example, here's a simple visualization of the displacement (u, v) and
-# correlation cost for the two deformed images using matplotlib. You'll need to
-# ensure you have `matplotlib.pyplot` installed and imported.
-fig, axes = plt.subplots(2, 3, figsize=(15, 10))
+# And finally a simple plot of the displacements and correlation
+# coefficient.
+fig, axes = plt.subplots(1, 3, figsize=(15, 10))
 axes = axes.flatten()
 
 # First deformation image
@@ -85,35 +97,32 @@ im1 = axes[0].pcolor(dicdata.ss_x, dicdata.ss_y, dicdata.u[0])
 im2 = axes[1].pcolor(dicdata.ss_x, dicdata.ss_y, dicdata.v[0])
 im3 = axes[2].pcolor(dicdata.ss_x, dicdata.ss_y, dicdata.cost[0])
 
-# Second deformation image
-im4 = axes[3].pcolor(dicdata.ss_x, dicdata.ss_y, dicdata.u[1])
-im5 = axes[4].pcolor(dicdata.ss_x, dicdata.ss_y, dicdata.v[1])
-im6 = axes[5].pcolor(dicdata.ss_x, dicdata.ss_y, dicdata.cost[1])
-
 # Titles
-axes[0].set_title('u component (def0000.tiff)')
-axes[1].set_title('v component (def0000.tiff)')
-axes[2].set_title('cost (def0000.tiff)')
-axes[3].set_title('u component (def0001.tiff)')
-axes[4].set_title('v component (def0001.tiff)')
-axes[5].set_title('cost (def0001.tiff)')
+axes[0].set_title('u component')
+axes[1].set_title('v component')
+axes[2].set_title('cost')
 
-for aa in axes:
-    aa.set_aspect('equal')
+# Axis ticks
+ticks = np.arange(0, 10001, 2500)
+for ax in axes:
+    ax.set_aspect('equal')
+    ax.set_xlim(0, 10000)
+    ax.set_ylim(0, 10000)
+    ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
+
 
 # Colorbars
-fig.colorbar(im1, ax=axes[0])
-fig.colorbar(im2, ax=axes[1])
-fig.colorbar(im3, ax=axes[2])
-fig.colorbar(im4, ax=axes[3])
-fig.colorbar(im5, ax=axes[4])
-fig.colorbar(im6, ax=axes[5])
+fig.colorbar(im1, ax=axes[0], fraction=0.046, pad=0.04)
+fig.colorbar(im2, ax=axes[1], fraction=0.046, pad=0.04)
+fig.colorbar(im3, ax=axes[2], fraction=0.046, pad=0.04)
 
 plt.tight_layout()
+plt.savefig("hrdic.png", dpi=300, bbox_inches="tight")
 plt.show()
 
 # %%
-# .. image:: ../../../../_static/plate_with_hole.png
+# .. image:: ../../../../_static/hrdic.png
 #    :alt: Displacement and cost values
 #    :width: 800px
 #    :align: center
