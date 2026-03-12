bb82cd2a337bfd4eea9c9632f6c9cf84e71a7de3
JoelPhys <jrhirst100@aol.com>
2026-03-12 14:12:03 +0000

    images are ordered l0,l1,...,ln,r0,r1,r2,r3,...rn.

--- src/pyvale/dic/dic3d.py
+++ src/pyvale/dic/dic3d.py
@@ -149,17 +149,17 @@ def calculate_3d(reference: list[np.ndarray] | list[str] | list[Path],
     filenames = filenames0 + filenames1
 
 
-    # sort it so that images go l0,r0,l1,r1,l2,r2 ....
-    # Interleave so images go l0,r0,l1,r1,l2,r2,...
-    n = image_stack0.shape[0]
-
-    # Interleave image stacks: shape goes from (n, ...) to (2n, ...)
-    image_stack = np.empty((2 * n, *image_stack0.shape[1:]), dtype=image_stack0.dtype)
-    image_stack[0::2] = image_stack0  # even indices: left
-    image_stack[1::2] = image_stack1  # odd indices:  right
-
-    # Interleave filenames
-    filenames = [name for pair in zip(filenames0, filenames1) for name in pair]
+    # # sort it so that images go l0,r0,l1,r1,l2,r2 ....
+    # # Interleave so images go l0,r0,l1,r1,l2,r2,...
+    # n = image_stack0.shape[0]
+    #
+    # # Interleave image stacks: shape goes from (n, ...) to (2n, ...)
+    # image_stack = np.empty((2 * n, *image_stack0.shape[1:]), dtype=image_stack0.dtype)
+    # image_stack[0::2] = image_stack0  # even indices: left
+    # image_stack[1::2] = image_stack1  # odd indices:  right
+    #
+    # # Interleave filenames
+    # filenames = [name for pair in zip(filenames0, filenames1) for name in pair]
 
     dicchecks.check_correlation_criteria(correlation_criteria)
     dicchecks.check_interpolation(interpolation_routine)
