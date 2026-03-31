bc3deb9d21d79fb382e4220222332c50d3a1a1a7
Chris Dawson <chrisdawson1996@gmail.com>
2026-03-31 11:02:38 +0100

    implement some of the cost function, fix radial return

--- src/pyvale/vfm/__init__.py
+++ src/pyvale/vfm/__init__.py
@@ -4,6 +4,6 @@
 # Copyright (C) 2025 The Computer Aided Validation Team
 #===============================================================================
 
-from .mechanical_properties import MechanicalProperties
-from .radial_return import radial_return
-from .stress_sensitivity import calculate_stress_sensitivity
+# from .mechanical_properties import MechanicalProperties
+# from .radial_return import radial_return
+# from .stress_sensitivity import calculate_stress_sensitivity
--- src/pyvale/vfm/global_virtual_fields_cost_function.py
+++ src/pyvale/vfm/global_virtual_fields_cost_function.py
@@ -1,14 +1,66 @@
+import numpy as np
+import numpy.typing as npt
 
+from pyvale.vfm.mechanical_properties import (
+    EDOFLabel,
+    EParameterLabel,
+)
+from pyvale.vfm.sensitivity_based_virtual_fields import (
+    SensitivityBasedVirtualFields,
+)
 
-Combine:
-globalVirtualFieldObjectiveFunction.m
-globalVirtualFieldCostFunction.m
-
+# Combine:
+# globalVirtualFieldObjectiveFunction.m
+# globalVirtualFieldCostFunction.m
 
 
 ## Define virtual fields (either update or use existing)
 
 # sensitivity based virtual fields or manual virtual fields
 
-
 ## Evaluate cost function
+
+# Inputs:
+#  - stress
+#  - our SBVFs dict[EParameterLabel, list[dict[EDOFLabel, SensitivityBasedVirtualFields]]]
+#  - force
+#  - area
+#  - thickness
+# Outputs:
+#  - scalar float cost?
+def global_vf_cost_function(
+    stress: npt.NDArray[np.float64], #(timesteps, components, y, x)
+    sensitivity_based_virtual_fields: dict[
+        EParameterLabel,
+        list[dict[EDOFLabel, SensitivityBasedVirtualFields]]
+    ],
+    force: npt.NDArray[np.float64], #(timesteps, components)
+    # TODO: is this the right form for area? or can we take a scalar and blow it up to size
+    area: npt.NDArray[np.float64], #(timesteps, components, y, x)
+    thickness: float
+):
+    # For each dof
+    #  - calculate internal virtual work
+    #  - calculate external virtual work
+
+    sbvfs = [
+        sbvf
+        for parameterisations in sensitivity_based_virtual_fields.values()
+        for p in parameterisations
+        for sbvf in p.values()
+    ]
+
+    for sbvf in sbvfs:
+        internal_virtual_work = (
+            stress * sbvf.virtual_strain * area * thickness
+        )
+
+        # This is the edge that the clamp force acts on
+        # TODO: pull this out of some config object? maybe
+        # dic_config should exist to store this kind of thing?
+        edge = 0
+
+        external_virtual_work = (
+            (force[:, 0] * sbvf.virtual_displacement[:, 0, edge])
+            + (force[:, 1] * sbvf.virtual_displacement[:, 1, edge])
+        )
