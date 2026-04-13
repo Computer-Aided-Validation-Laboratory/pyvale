b62a55f4be245490d8fb5818c9c8181caba3b067
Rob Hamill <40853079+rob-hamill@users.noreply.github.com>
2026-04-13 11:19:05 +0100

    overhaul of vfm code implementing the identification manage (phase state manager) and general scheme of parameterisations and metrics. some remaining bugs and clunkiness

---
+++ src/pyvale/vfm/identification_linear.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from pyvale.vfm.project_definition import PhaseDefinition, PhaseResult, TestData
+
+
+def run_linear_identification(
+    test_data: TestData,
+    phase_definition: PhaseDefinition,
+):
+    raise NotImplementedError(
+        "Elastic / linear identification is planned but not implemented yet."
+    )
+
