8517cfbe4cec35902a579879276d3237f90c0441
Chris Dawson <chrisdawson1996@gmail.com>
2026-05-20 10:31:59 +0100

    add scalar/vector first result passthrough

---
+++ src/pyvale/vfm/objective_functions/vector_first_result_passthrough.py
@@ -0,0 +1,12 @@
+import numpy as np
+import numpy.typing as npt
+from pyvale.vfm.objective_functions.objective_function import (
+    IVectorObjectiveFunction,
+)
+
+class VectorFirstResultPassthrough(IVectorObjectiveFunction):
+    def evaluate(
+        self,
+        metric_results: list[npt.NDArray[np.float64]],
+    ) -> npt.NDArray[np.float64]:
+        return metric_results[0]
