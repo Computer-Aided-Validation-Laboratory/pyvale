fd726ab2bed2a9643b0b969620751200cd95f921
JoelPhys <jrhirst100@aol.com>
2026-06-04 13:52:10 +0100

    initial filename rejig for stereo strain caluclations.

--- src/pyvale/strain/__init__.py
+++ src/pyvale/strain/__init__.py
@@ -4,10 +4,12 @@
 # Copyright (C) 2025 The Computer Aided Validation Team
 #===============================================================================
 
-from .strain import calculate_2d
+from .strain2d import calculate_2d
+from .strain3d import calculate_3d
 from .strainimport import import_2d
 from .strainresults import StrainResults
 
 __all__ = ["calculate_2d",
+           "calculate_3d",
            "StrainResults",
            "import_2d"]
