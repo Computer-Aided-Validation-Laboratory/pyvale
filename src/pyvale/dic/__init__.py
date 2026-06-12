93161672f95d776ed3958defa63d9e5e7f783ee6
JoelPhys <jrhirst100@aol.com>
2026-06-12 15:49:08 +0100

    added import_3d to the DIC __init__

--- src/pyvale/dic/__init__.py
+++ src/pyvale/dic/__init__.py
@@ -7,6 +7,7 @@
 from .dic2d import calculate_2d
 from .dic3d import calculate_3d
 from .dicimport2d import import_2d
+from .dicimport3d import import_3d
 from .dicregionofinterest import RegionOfInterest
 from .dicresults import Results
 from .diccpp import Bspline, Interpolator
@@ -16,6 +17,7 @@ __all__ = ["calculate_2d",
            "calculate_3d",
            "RegionOfInterest",
            "import_2d",
+           "import_3d",
            "Bspline",
            "Intepolator",
            "Results"]
