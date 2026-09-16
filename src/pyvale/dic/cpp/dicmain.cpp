5b735649b42d4144cdddd14a5d8a644bb0e8084c
JoelPhys <jrhirst100@aol.com>
2026-09-16 14:53:34 +0100

    left camera deformation results now passed to stereo for SINGLEWINDOW_RG

--- src/pyvale/dic/cpp/dicmain.cpp
+++ src/pyvale/dic/cpp/dicmain.cpp
@@ -304,7 +304,8 @@ void engine_impl(const py::array_t<bool>& img_roi_arr,
                                     results_ref_l,
                                     results_def_r,
                                     "stereo",
-                                    stereo_geom.F);
+                                    stereo_geom.F,
+                                    results_def_l);
 
                     stereo::pixel_to_world(ss_grid_l_0,
                                         calib,
